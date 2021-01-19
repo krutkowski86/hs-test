879111b3e8d80ccca32c25390097d2fd05366b64
krutkowski86 <krzysztof.rutkowski.86@gmail.com>
2021-01-19 23:51:59 +0100

    route wild card - page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './core/components/login/login.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -10,7 +11,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('src/app/auth/auth.module').then((m) => m.AuthModule)
-  }
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
---
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PageNotFoundComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
