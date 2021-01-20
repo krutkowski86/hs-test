c8af8b45b2653e764796246c4464b06c990f96e8
krutkowski86 <krzysztof.rutkowski.86@gmail.com>
2021-01-20 01:40:52 +0100

    snack on auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -4,17 +4,24 @@ import { of } from 'rxjs';
 import { concatMap, tap } from 'rxjs/operators';
 
 import { AuthService } from '../services/auth.service';
+import { SnackService } from '../services/snack.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanLoad {
-  constructor(private _authService: AuthService, private _router: Router) {}
+  constructor(private _authService: AuthService, private _router: Router, private _snackService: SnackService) {}
 
   canLoad() {
-    return this.isAuthenticated('canLoad').pipe(concatMap(this.loginOnFalse.bind(this)));
+    return this.isAuthenticated('canLoad').pipe(
+      tap(this.snackOnFalse.bind(this)),
+      concatMap(this.loginOnFalse.bind(this))
+    );
   }
 
   canActivate() {
-    return this.isAuthenticated('canActivate').pipe(concatMap(this.loginOnFalse.bind(this)));
+    return this.isAuthenticated('canActivate').pipe(
+      tap(this.snackOnFalse.bind(this)),
+      concatMap(this.loginOnFalse.bind(this))
+    );
   }
 
   private isAuthenticated(guardType: string) {
@@ -25,5 +32,13 @@ export class AuthGuard implements CanActivate, CanLoad {
 
   private loginOnFalse(authenticated: boolean) {
     return authenticated ? of(true) : of(this._router.createUrlTree(['/login']));
+  }
+
+  private snackOnFalse(authenticated) {
+    console.log(authenticated);
+    if (!authenticated) {
+      console.log('not');
+      this._snackService.warnMsg('User not authenticated');
+    }
   }
 }
