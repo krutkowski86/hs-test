b830431ba34c2961726310ac4db6d0e484b39152
krutkowski86 <krzysztof.rutkowski.86@gmail.com>
2021-01-20 01:15:41 +0100

    logout service

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {
-  constructor() {}
+export class HeaderComponent implements OnInit {
+  authenticated: boolean;
+  constructor(private _authService: AuthService, private _router: Router, private _cd: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this._authService.userState$.subscribe(({ authenticated }) => {
+      this.authenticated = authenticated;
+      this._cd.detectChanges();
+    });
+  }
+
+  logout() {
+    this._authService.logout().subscribe((out) => {
+      if (out) {
+        this._router.navigate(['/login']);
+      }
+    });
+  }
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,24 +1,40 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject, of } from 'rxjs';
+import { first, map, tap } from 'rxjs/operators';
+
+interface UserState {
+  authenticated: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private authenticated: boolean;
+  private userStateSource = new BehaviorSubject<UserState>({ authenticated: false });
+  userState$ = this.userStateSource.asObservable();
 
   constructor() {}
 
   login() {
-    return this.loginUser().pipe(tap(() => (this.authenticated = true)));
+    return this.loginUserRequest().pipe(tap(() => this.userStateSource.next({ authenticated: true })));
+  }
+
+  logout() {
+    return this.logoutUserRequest().pipe(tap(() => this.userStateSource.next({ authenticated: false })));
   }
 
   isAuthenticated() {
-    return of(!!this.authenticated);
+    return this.userState$.pipe(
+      first(),
+      map(({ authenticated }) => authenticated)
+    );
   }
 
-  private loginUser() {
+  private loginUserRequest() {
+    return of(true);
+  }
+
+  private logoutUserRequest() {
     return of(true);
   }
 }
